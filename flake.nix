adeeae4606672b3e1919edb1d9b6390aa2aaea4a
Will McKinnon <contact@willmckinnon.com>
2022-08-24 00:25:40 -0400

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "chip-8 interpreter development environment";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = {self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            gcc gnumake
+          ];
+
+          buildInputs = with pkgs; [
+            ncurses
+          ];
+        };
+      }
+    );
+}
